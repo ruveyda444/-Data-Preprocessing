70275a8adcefb6ced0092bfca4295255cf4021ab
Rüveyda <ruveyda@getheltia.com>
2025-07-29 20:45:16 +0300

    1. Data preprocessing

--- first_data.py
+++ first_data.py
@@ -1,10 +1,19 @@
 import seaborn as sns
 
-tips = sns.load_dataset("tips")
-df= tips.copy()
+tips = sns.load_dataset("tips") 
+df= tips.copy()  # kopyalama
 
-df.head()
+print(df.head()) # ilk 5 satır
 
-df.info()
+df.info() #  sütun bilgileri
 
+print(df.tail())  # son 5 satır
 
+print(df.describe())  # sayısal sütunların istatistiksel bilgileri
+
+print(df.describe().T)  # transpoze etme
+
+df.rename(columns={"total_bill": "fiyat", "tip": "bahşiş", "sex": "cinsiyet", "smoker": "sigara", "time": "zaman",
+                   "size": "kişiler"}, inplace=True)  # sütun adlarını değiştirme
+
+print(df.head()) 
